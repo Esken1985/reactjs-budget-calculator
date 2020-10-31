1011e0faee15f250a472e65ba3ecd14d6770dc90
Eskender <esken999@gmail.com>
2020-10-31 19:55:19 +0300

    setting up the ExpenseList component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import ExpenseItem from "./ExpenseItem"
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
 
-const ExpenseList = () => {
-    return (
-        <div>
-            <ExpenseItem />
-            ExpenseList Component
-        </div>
-    )
-}
+const ExpenseList = ({ expenses }) => {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return <ExpenseItem key={expense.id} expense={expense} />;
+        })}
+      </ul>
+      {expenses.length > 0 && (
+        <button className="btn">
+          clear expenses
+          <MdDelete className="btn-icon" />
+        </button>
+      )}
+    </>
+  );
+};
 
-export default ExpenseList
+export default ExpenseList;
