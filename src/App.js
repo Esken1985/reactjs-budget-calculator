899c29a4ecf78ac5b60b0181996c037a8456179b
Eskender <esken999@gmail.com>
2020-11-01 06:39:47 +0300

    adding Alert component, setting up submit and alert functionality

--- src/App.js
+++ src/App.js
@@ -12,13 +12,57 @@ const initialExpenses = [
 ];
 
 function App() {
+  // ********* state values **********
   const [expenses, setExpenses] = useState(initialExpenses);
+
+  // ******** functionality **********
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState("");
+  const [alert, setAlert] = useState({ show: false });
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  // handle alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 5000);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const singleExpense = {
+        id: uuidv4(),
+        charge,
+        amount,
+      };
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+      handleAlert({ type: "success", text: "item added" });
+    } else {
+      handleAlert({ type: "danger", text: "no items to add" });
+    }
+  };
+
   return (
     <>
-      <Alert />
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <h1>budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
@@ -26,7 +70,7 @@ function App() {
         <span className="total">
           ${" "}
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
